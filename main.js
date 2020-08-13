a8f6c7f5384e92caea83ff75382c026be48ffdf1
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-13 22:48:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const cardPanel = document.getElementById("card-panel");
-
+let selectedCard = {
+    className: null,
+    value: null,
+  },
+  selectedSecondCard = {
+    className: null,
+    value: null,
+  };
 //Kartları, kart paneline grid düzenine göre yerleştiren IIFE fonksiyonu
 (function () {
   let rows = 3,
-    columns = 5;
+    columns = 6;
   cardPanel.style.setProperty("--grid-rows", rows);
   cardPanel.style.setProperty("--grid-cols", columns);
   for (c = 0; c < rows * columns; c++) {
     let card = document.createElement("div");
-    cardPanel.appendChild(card).className = "card-item";
+    cardPanel.appendChild(card).className = `card-item card-${c}`;
   }
 })();

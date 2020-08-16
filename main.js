4b4b8541afc776e25bb28163b864c659e67f41ef
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-16 10:50:29 +0300

    CardsValueArr and assignCardValue( )  prepared.

--- main.js
+++ main.js
@@ -7,14 +7,31 @@ let selectedCard = {
     className: null,
     value: null,
   };
-//Kartları, kart paneline grid düzenine göre yerleştiren IIFE fonksiyonu
-(function () {
-  let rows = 3,
-    columns = 6;
-  cardPanel.style.setProperty("--grid-rows", rows);
-  cardPanel.style.setProperty("--grid-cols", columns);
-  for (c = 0; c < rows * columns; c++) {
-    let card = document.createElement("div");
-    cardPanel.appendChild(card).className = `card-item card-${c}`;
-  }
-})();
+let cardValue = null,
+  cardsValueArr = [...Array(18)],
+  cardList = [...Array(18)],
+  score = 0,
+  health = 50;
+
+/**
+ * Panelin sıra değerlerini random alabilmek için
+ * cardsValueArr dizisine panelin sıra değerlerini
+ * atadık.
+ */
+for (let a = 0; a < 18; a++) {
+  cardsValueArr[a] = a;
+}
+
+/**
+ * assignCardValue() random değerler ile hazırlanan
+ * cardList array'ine yeni kart değeri ekler
+ */
+function assignCardValue(cardValue) {
+  let orderNum = Math.floor(Math.random() * cardsValueArr.length);
+
+  orderNum = cardsValueArr[orderNum];
+  cardList[orderNum] = cardValue;
+
+  let index = cardsValueArr.indexOf(orderNum);
+  cardsValueArr.splice(index, 1);
+}

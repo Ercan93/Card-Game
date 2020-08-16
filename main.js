b62210905a8d774df33eb7318615029a6c5a0d07
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-16 12:24:42 +0300

    addSelectedCardData( ) prepared.
    and bit more comment.

--- main.js
+++ main.js
@@ -24,14 +24,21 @@ for (let a = 0; a < 18; a++) {
 
 /**
  * assignCardValue() random değerler ile hazırlanan
- * cardList array'ine yeni kart değeri ekler
+ * cardList array'ine yeni kart değeri ekler.
+ *  @param {String} cardValue - Kartın posterIndex'ni ifade eder.
  */
 function assignCardValue(cardValue) {
+  // cardsValueArr içindeki sıralardan random bir sıra alınır.
   let orderNum = Math.floor(Math.random() * cardsValueArr.length);
 
+  //orderNum sırasının barındırğı panel sırası orderNum'a atılır.
   orderNum = cardsValueArr[orderNum];
+  // cardList'te panel sırasına kartın değeri atılır.
   cardList[orderNum] = cardValue;
 
+  /* panel sırası değeri cardValueArr dizisinden çıkarılır,
+  böylelikle birr sıraya iki değer atanması önlenir.
+  */
   let index = cardsValueArr.indexOf(orderNum);
   cardsValueArr.splice(index, 1);
 }
@@ -60,26 +67,30 @@ function updateHealthPanel() {
  */
 function createRandomList() {
   for (let i = 0; i < 18; i++) {
+    // Her kartın bir eşi olduğundan dolayı kontrollü atama yapılır.
     if (i % 2 == 0) cardValue = Math.floor(Math.random() * 10);
     assignCardValue(cardValue);
   }
 }
+
 /**
- * addCardsToPanel() kartları panele
- * grid düzenine göre dizer.
+ * Kartları panele grid düzenine göre dizer.
  */
 function addCardsToPanel() {
   let rows = 3,
     columns = 6;
+  // Kart panelinin satırlarını css'e atar.
   cardPanel.style.setProperty("--grid-rows", rows);
+  // Kart panelinin sütunlarını css'e atar.
   cardPanel.style.setProperty("--grid-cols", columns);
 
+  //Kartları oluşturup cardPanel elementinin içine yazar.
   for (c = 0; c < rows * columns; c++) {
     let card = document.createElement("div");
     cardPanel.appendChild(card).className = `card-item card-${c}`;
 
     //Kartların eşleşme durumunu kontrol etmek
-    //için data-index-number attribute'u oluşturduk
+    //için data-index-number attribute'u oluşturduk.
     card.dataset.indexNumber = c;
   }
   createRandomList();
@@ -88,3 +99,21 @@ function addCardsToPanel() {
 }
 
 addCardsToPanel();
+
+/**
+ *  Seçilen kartın değerlerini element'in referans olduğu objeye atar.
+ * @param {Object} element - Kart değerlerinin saklanacağı obje.
+ * @param {String} element.className - Seçilen kartın class'ını saklar.
+ * @param {String} element.value - Seçilen kartın posterIndex'ini saklar.
+ * @param {String} className - Seçilen kartın tüm class'larını gösterir.
+ * @param {String} posterIndex - Seçilen kartın isim index'i.
+ */
+function addSelectedCardData(element, className, posterIndex) {
+  element.className = "." + className.split(" ")[1];
+  element.value = posterIndex;
+  // Seçilen kartın posterini div'in içinde gösterir.
+  $(element.className).css(
+    "background-image",
+    `url('./assets/images/poster-${posterIndex}.jpg')`
+  );
+}
